761a3d5d2b13061ef9adcf187bfcb0a96ab13748
MxChamp24 <MxFan24@gmail.com>
2013-11-07 13:28:35 -0500

    Executed the Useless Button in Gameplay
    I removed the button in the top left of the gameplay screen because we
    are going to use the game summary to restart our game.

--- TEAMWORK/Assets/GameController.cs
+++ TEAMWORK/Assets/GameController.cs
@@ -22,18 +22,5 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	//This wasn't here before
-	}
-	
-	void OnGUI () {
-		// Make a background box
-		GUI.Box(new Rect(10,10,100,90), "Start Menu");
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Play Game")) {
-			Application.LoadLevel(1);
-		}
-	}
-	
-	
-	
+	}	
 }
